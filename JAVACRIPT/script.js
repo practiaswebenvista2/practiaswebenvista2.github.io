d93c08fbaf7999aabba16878f56968df2a14806a
practiaswebenvista2 <127597862+practiaswebenvista2@users.noreply.github.com>
2023-04-14 14:06:32 +0200

    V3.0

--- JAVACRIPT/script.js
+++ JAVACRIPT/script.js
@@ -1,4 +1,13 @@
-
+/*CAMBIO ICONO MENU HAM*/
 function cambiarIcono(x){
   x.classList.toggle("fa-xmark");
+}
+
+/*MENU ACORDEÓN*/
+const accordion = document.getElementsByClassName('container');
+
+for (i=0; i<accordion.length; i++) {
+  accordion[i].addEventListener('click', function () {
+    this.classList.toggle('active')
+  })
 }
